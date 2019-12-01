1c569e76acbd2f16d9d7c30552f3ea0dd6d27b9d
Michel Neves <nevess.michel@gmail.com>
2019-12-01 17:43:54 -0300

    visualizar ultimo erro pela rota /test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os, sys, requests
-sys.path.append('./venv/lib/python3.6/site-packages')
+#sys.path.append('./venv/lib/python3.6/site-packages')
 
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
@@ -26,17 +26,26 @@ def verify():
 #Rota de recebimento de mensagens
 @app.route('/', methods=['POST'])
 def webhook():
+	global message
 	#Pegas os dados do request em formato Json
 	data = request.get_json()
-	req = requests.post("http://35.236.87.50:8080/", json=data)
+	#print(data)
+	try:
+		req = requests.post("http://34.95.151.238/chat/", json=data)
+		#print(req.status_code)
+		#print(req.json())
+	except Exception as e:
+		message = e
+
 	return "OK", 200
 
 @app.route('/test')
 def test_http():
+	global message
 	return message
 
 
 if(__name__ == '__main__'):
 	app.threaded = True
-	app.port = 8080
+	app.port = 5000
 	app.run(host="0.0.0.0", debug=True)
